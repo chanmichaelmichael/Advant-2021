59049cd09c4d77125111ed1f798c5e271b3adc1b
Michael Chan <michael.ta.chan@gmail.com>
2022-04-12 22:36:12 -0700

    day 11

--- 11.py
+++ 11.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 
 print("Day 11 Part 1: ")
-with open("11-inputs-test.txt") as f:
+with open("11-inputs.txt") as f:
     ### turn inputs into 2d array
     rawInputs = [line.split("\n")[0] for line in f.readlines()]
     
@@ -16,20 +16,13 @@ with open("11-inputs-test.txt") as f:
         inputs.append([int(char) for char in line])
     
     inputsArray = np.array(inputs)
-    print(inputsArray, "\n")
+    #print(inputsArray, "\n")
     
     lastRow = len(inputsArray) - 1
     lastColumn = len(inputsArray[0]) - 1
     
     numRows = len(inputsArray)
     numColumns = len(inputsArray[0])
-    
-    
-    
-    ### loops?
-    numLoops = 2 #test with 10 loops first 
-    flashes = 0 #output
-    group = copy.deepcopy(inputsArray)
     
     
     
@@ -117,24 +110,30 @@ with open("11-inputs-test.txt") as f:
     ### flash adjacent octopus
     def flashAdj(i, j):
         adj = neighbors(i, j)
+        temp = 0
         for neighbor in adj:
             #already flashed
             if group[neighbor[0], neighbor[1]] == 0: 
-                return 0
+                continue
             #going to flash
             elif group[neighbor[0], neighbor[1]] == 9: 
-                temp = 1
+                temp = temp + 1
                 group[neighbor[0], neighbor[1]] = 0
                 temp += flashAdj(neighbor[0], neighbor[1])
-                return temp
             #nothing
             else: 
-                group[neighbor[0], neighbor[1]] = group[neighbor[0], neighbor[1]] + 1
-                return 0
+                group[neighbor[0], neighbor[1]] += 1
+                continue
+        return temp
     
     ### MAIN FUNCTION
+    ### loops?
+    numLoops = 100
+    flashes = 0 #output
+    group = copy.deepcopy(inputsArray)
+    
     for loop in range(numLoops):
-        print("Step ", loop)
+        #print("Step ", loop)
         ###first, energy level of all octopus increases by 1
         group = group + 1
         #print("Number of add'l flashes: ", len(group[group == 10])) 
@@ -146,13 +145,15 @@ with open("11-inputs-test.txt") as f:
         flashes += len(flashers[0]) #initial flash
         group[flashers] = 0
         
-        print("x: ", flashers[0])
-        print("y: ", flashers[1])
+        #print("x: ", flashers[0])
+        #print("y: ", flashers[1])
+        
         
         for i in range(len(flashers[0])):
             #x is flashers[0][i]
             #y is flashers[1][i]
             flashes = flashes + flashAdj(flashers[0][i], flashers[1][i])
+        
         
         """
         #REMEMBER TO CHANGE ALL 10s to 0s at the end        
@@ -161,24 +162,55 @@ with open("11-inputs-test.txt") as f:
         print("post reset to 0: ", group, "\n\n")
         """
         
-        print("Total Flashes: ", flashes)        
-        print(group, "\n")
+        #print("Total Flashes: ", flashes)        
+        #print(group, "\n")
     
     print("Final Group")
     print(group, "\n")
     #print("x: ", flashers[0][0])
     #print("y: ", flashers[1][0])
-    
-    
+    print("Total Flashes: ", flashes)  
     
     part1End = time.time()
-    print("\tPart 1 runtime: ", part1Start - part1End, "\n")
+    print("\tPart 1 runtime: ", part1End - part1Start, "\n")
+    
+    
     
     
     ##### PART 2 #####
     print("Day 11 Part 2: ")
     part2Start = time.time()
     
+    group = copy.deepcopy(inputsArray)
+    stop = False
+    timeToSync = 0
+    flashes = 0
+    ### MAIN FUNCTION
+    while not stop:
+        timeToSync += 1
+
+        ###first, energy level of all octopus increases by 1
+        group = group + 1
+        
+        ###all the ones whose neighbors we need to affect
+        flashers = np.where(np.isin(group, 10))
+        flashes += len(flashers[0]) #initial flash
+        group[flashers] = 0
+
+        for i in range(len(flashers[0])):
+            #x is flashers[0][i]
+            #y is flashers[1][i]
+            flashes = flashes + flashAdj(flashers[0][i], flashers[1][i])
+        
+        syncStage = np.where(np.isin(group, 0))
+        #print(len(syncStage[0]))
+        if len(syncStage[0]) == 100:
+            stop = True
+
+    #print("Final Group")
+    #print(group, "\n")
+    print("Number of Loops to Sync: ", timeToSync)  
+    
     
     part2End = time.time()
-    print("\tPart 2 runtime: ", part2Start - part2End, "\n")
+    print("\tPart 2 runtime: ", part2End - part2Start, "\n")
